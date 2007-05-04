1de3762fc12b6b8791174be01da74569ebaf88ac
cmsbuild <>
2007-05-04 09:17:56 +0000

    This commit was manufactured by cvs2git to create tag 'HLTrigger- btau-V00-00-50'.
    Sprout from master 2007-05-04 09:17:55 UTC wilke <sha1-33db0e9bddc44d2427b8ee46baf08bbe956a435f@cern.ch> 'corrected calculation of decay length error'
    Cherrypick from master 2007-04-07 08:50:55 UTC Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch> '*** empty log message ***':
        data/jetTag/bHLTjetmu.cff
        src/SealModule.cc
        test/BuildFile
        test/GetData.cc
    Cherrypick from master 2007-03-26 15:16:39 UTC Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch> '*** empty log message ***':
        test/hlt_tau.cfg
    Cherrypick from master 2007-04-17 13:00:51 UTC Ian Tomalin <sha1-3fa43f967df83df8ba717ce5a271c8449420497d@cern.ch> 'Committed code for 1_3_X_HLT':
        data/jetTag/L2multiJet.cff
    Cherrypick from master 2007-04-24 16:47:22 UTC Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch> 'latest for 1.3.X':
        BuildFile
    Delete:
        data/jetTag/bHLTassJetMu.cff

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -1,4 +1,4 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "HLTrigger/btau/interface/HLTJetTag.h"
