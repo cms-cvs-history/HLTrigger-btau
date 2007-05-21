ead01c192eb7fb19232a8e168f1e1f55da2145ac
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2007-05-21 14:20:02 +0000

    updated to V00-00-69

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -5,10 +5,12 @@
 #include "HLTrigger/btau/interface/HLTTauL25DoubleFilter.h"
 #include "HLTrigger/btau/interface/HLTDisplacedmumuFilter.h"
 #include "HLTrigger/btau/interface/HLTL1MuonCorrector.h"
+#include "HLTrigger/btau/interface/GetJetsFromHLTobject.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HLTJetTag);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauL25DoubleFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTDisplacedmumuFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTL1MuonCorrector);
+DEFINE_ANOTHER_FWK_MODULE(GetJetsFromHLTobject);
 
